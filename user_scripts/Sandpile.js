642b954ed93884380ae9b5d943f36138f93f05b9
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-23 16:45:17 -0500

    Update Sandpile.js

--- user_scripts/Sandpile.js
+++ user_scripts/Sandpile.js
@@ -1,8 +1,8 @@
-function NewBrick(XSize, YSize, ZSize, XPos, YPos, ZPos, Tras, Color) {
-    let brick = new Brick(new Vector3(XPos, YPos, ZPos), new Vector3(XSize, YSize, ZPos), Color)
+function NewBrick(XSize, YSize, ZSize, XPos, YPos, ZPos, Tras) {
+    let brick = new Brick(new Vector3(XPos, YPos, ZPos), new Vector3(XSize, YSize, ZPos), "#CECECE")
     brick.visibility = (Tras)
 
     Game.newBrick(brick) // "Parent" the brick to the game so players will download it.
 
 }
-console.log(NewBrick(1,1,1,1,1,1,1,#000000));
+console.log(NewBrick(1,1,1,1,1,1,1,));
