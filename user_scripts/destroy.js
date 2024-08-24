3e4ee50b9cf74cb11f543e03a4127c90d88745e2
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-24 08:24:46 -0500

    Update destroy.js

--- user_scripts/destroy.js
+++ user_scripts/destroy.js
@@ -1,1 +1,11 @@
+const brick = world.bricks.find(brick => brick.name === brick)
+const destroy = new Tool("Destroy")
+Game.on("initialSpawn",p=>{
+    p.addTool(destroy)
+  })
 
+  tool.on("equipped",p=>{
+    brick.clicked((player, secure) => {
+        if (!secure) return
+    })
+  })
