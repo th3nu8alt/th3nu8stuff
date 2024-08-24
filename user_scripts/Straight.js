e192c0e022ac080949092fce7bbb066c8eff4cb0
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-23 20:33:12 -0500

    Update Straight.js

--- user_scripts/Straight.js
+++ user_scripts/Straight.js
@@ -1,29 +1,9 @@
-active = 1
-const tool = new Tool("Straight")
+const tool = new Tool("test")
 Game.on("initialSpawn",p=>{
   p.addTool(tool)
 })
-function NewBrick(size,pos,trans,color) {
-    let brick = new Brick(pos,size,color)
-    brick.visibility = trans
-    
-    Game.newBrick(brick)
-  }
-  
-  function LoopStraight() {
-    NewBrick(new Vector3(1,1,1),p.position,0.5,"#000000")
-    destroy()
-    if(active == 0) {
-        LoopStraight()
-    }
-  }
-  
-  tool.on("equipped",p=>{
-    active=0
-    LoopStraight()
-  })
 
 tool.on("activated",p=>{
     NewBrick(new Vector3(1,1,1),p.poition,1,"#000000")
-    active = 1
+    setPosition(new Vector3(Math.round(x/8)*8, Math.round(y/8)*8, Math.round(z/8)*8))
 })
