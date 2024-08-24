abaf77022f143777fd14cfb652803bf0c904b334
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-24 08:32:45 -0500

    Update Straight.js

--- user_scripts/Straight.js
+++ user_scripts/Straight.js
@@ -10,7 +10,7 @@ function roundTo(num,rnd) {
 
 
 tool.on("activated",p=>{
-    let brick = new Brick(p.position,new Vector3(4,4,4),"#589D3C")
+    let brick = new Brick(clickable = true,p.position,new Vector3(4,4,4),"#589D3C")
     Game.newBrick(brick)
     brick.setPosition(new Vector3(roundTo(brick.position.x,4)-2, roundTo(brick.position.y,4)-2, roundTo(brick.position.z,4)-4))
 
