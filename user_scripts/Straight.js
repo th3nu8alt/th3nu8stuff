ab0f3ea0bb0f7d0f85f7d407107b90be775409d4
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-23 19:21:11 -0500

    Update Straight.js

--- user_scripts/Straight.js
+++ user_scripts/Straight.js
@@ -1,15 +1,7 @@
-function NewBrick(XSize, YSize, ZSize, XPos, YPos, ZPos, Tras, Color) {
-    let brick = new Brick(new Vector3(XPos, YPos, ZPos), new Vector3(XSize, YSize, ZPos), "#000000")
-    brick.visibility = (Tras)
-
-    Game.newBrick(brick) // "Parent" the brick to the game so players will download it.
-
-}
 const tool = new Tool("Straight")
-tool.model = 655
 Game.on("initialSpawn",p=>{
   p.addTool(tool)
 })
-tool.on("activated",p=>{
-  Game.newBrick(new Brick(p.position,new Vector3(1,1,1),"#000000"))
+tool.on("equipped",p=>{
+  NewBrick(new Vector3(1,1,1),p.position,1,"#000000")
 })
