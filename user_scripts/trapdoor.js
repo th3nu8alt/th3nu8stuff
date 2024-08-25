8e53ce7eb1e37f2fbfccb0a42653b8110d5447f7
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-25 12:14:28 -0500

    Update trapdoor.js

--- user_scripts/trapdoor.js
+++ user_scripts/trapdoor.js
@@ -10,7 +10,7 @@ function roundTo(num,rnd) {
 tool.on("activated",p=>{
     let brick = new Brick(p.position,new Vector3(4,4,1),"#906847")
     Game.newBrick(brick)
-    brick.setPosition(new Vector3(roundTo(brick.position.x,4)-2, roundTo(brick.position.y,4)-2, roundTo(brick.position.z,4)-4))
+    brick.setPosition(new Vector3(roundTo(brick.position.x,4)-2, roundTo(brick.position.y,4)-2, roundTo(brick.position.z,4)))
     brick.clickable = true
     brick.collision = false
 }) 
