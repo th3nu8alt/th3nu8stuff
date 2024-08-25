483e8fd9918e38fa50b97d05b6df446ab6df4315
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-24 21:13:15 -0500

    Create door.js

---
+++ user_scripts/door.js
@@ -0,0 +1,16 @@
+const tool = new Tool("test")
+Game.on("initialSpawn",p=>{
+  p.addTool(tool)
+})
+
+function roundTo(num,rnd) {
+  return Math.round(num/rnd)*rnd
+}
+
+tool.on("activated",p=>{
+    let brick = new Brick(p.position,new Vector3(4,4,8),"#906847")
+    Game.newBrick(brick)
+    brick.setPosition(new Vector3(roundTo(brick.position.x,4)-2, roundTo(brick.position.y,4)-2, roundTo(brick.position.z,4)-4))
+    brick.clickable = true
+    brick.collision = false
+}) 
