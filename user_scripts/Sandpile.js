cc2855e35ca3a62b40a413ddb700afc082993092
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-24 19:59:08 -0500

    Update Sandpile.js

--- user_scripts/Sandpile.js
+++ user_scripts/Sandpile.js
@@ -1,3 +1,9 @@
+Game.on("initialSpawn",p=>{
+    p.setPosition(Vector3(-4,-307,2))
+    if(player.ownsAsset(690)) {
+        p.setPosition(Vector3(0,0,0))
+    }
+})
 function NewBrick(XSize, YSize, ZSize, XPos, YPos, ZPos, Tras, Color) {
     let brick = new Brick(new Vector3(XPos, YPos, ZPos), new Vector3(XSize, YSize, ZPos), "#000000")
     brick.visibility = (Tras)
@@ -8,7 +14,3 @@ function NewBrick(XSize, YSize, ZSize, XPos, YPos, ZPos, Tras, Color) {
 Game.command("clearplz", (caller, args) => {
     Game.clearMap(); Game.centerPrintAll("Bricks have been cleared.", 3)
 })
-p.setPosition(Vector3(-4,-307,2))
-if(player.ownsAsset(690)) {
-    p.setPosition(Vector3(0,0,0))
-}
