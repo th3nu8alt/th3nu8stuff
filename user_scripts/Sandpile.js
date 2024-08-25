5669e6a63c624803c8d1a142310898cfb2fc61e3
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-24 20:35:37 -0500

    Update Sandpile.js

--- user_scripts/Sandpile.js
+++ user_scripts/Sandpile.js
@@ -1,8 +1,6 @@
-Game.on("initialSpawn",(p)=>{
-    if (p.ownsAsset(690)) {
-        p.setPosition(new Vector3(0,0,0))
-    }else{
-        p.setPosition(new Vector3(-4,-307,2))
+Game.on("playerJoin", (p) => {
+    if !(p.ownsAsset(690)) {
+        p.kick("Buy the beta gamepass to join")
     }
 })
 function NewBrick(XSize, YSize, ZSize, XPos, YPos, ZPos, Tras, Color) {
