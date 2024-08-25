e761a0dc3fa88864c4618f16635854857750ef18
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-24 21:14:15 -0500

    Create slab.js

---
+++ user_scripts/slab.js
@@ -0,0 +1,15 @@
+const tool = new Tool("Slab")
+Game.on("initialSpawn",p=>{
+  p.addTool(tool)
+})
+
+function roundTo(num,rnd) {
+  return Math.round(num/rnd)*rnd
+}
+
+tool.on("activated",p=>{
+    let brick = new Brick(p.position,new Vector3(4,4,2),"#906847")
+    Game.newBrick(brick)
+    brick.setPosition(new Vector3(roundTo(brick.position.x,4)-2, roundTo(brick.position.y,4)-2, roundTo(brick.position.z,4)-4))
+    brick.clickable = true
+}) 
