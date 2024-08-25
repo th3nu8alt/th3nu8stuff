8e68c0eab7fe6e9a744c4cd5292915f23e988515
th3nu8 <blueboltgamingyt@gmail.com>
2024-08-24 21:18:36 -0500

    Update slab.js

--- user_scripts/slab.js
+++ user_scripts/slab.js
@@ -10,6 +10,6 @@ function roundTo(num,rnd) {
 tool.on("activated",p=>{
     let brick = new Brick(p.position,new Vector3(4,4,2),"#906847")
     Game.newBrick(brick)
-    brick.setPosition(new Vector3(roundTo(brick.position.x,4)-2, roundTo(brick.position.y,4)-2, roundTo(brick.position.z,4)-4))
+    brick.setPosition(new Vector3(roundTo(brick.position.x,4)-2, roundTo(brick.position.y,4)-2, roundTo(brick.position.z,4)))
     brick.clickable = true
 }) 
